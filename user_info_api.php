f74265bc8fccc6bd89f56a7b5c03e28d6212d893
rokynaasmind <roky.naasmind@gmail.com>
2026-06-11 04:10:44 +0600

    api update

--- user_info_api.php
+++ user_info_api.php
@@ -135,7 +135,7 @@ $foundUser = null;
 
 // try-1: uid = "10001"  (String)
 $where  = urlencode(json_encode(['uid' => $uid]));
-$result = b4aQuery($B4A_BASE . '/1231960377/users?where=' . $where . '&limit=1', $B4A_APP_ID, $B4A_MASTER_KEY);
+$result = b4aQuery($B4A_BASE . '/classes/_User?where=' . $where . '&limit=1', $B4A_APP_ID, $B4A_MASTER_KEY);
 print_r($result);
 die;
 if (!empty($result['results'])) {
